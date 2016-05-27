05dc30cfe781bcbfb3c8c7861a16025296dcca1a
Alessio <Alessio@iMac-di-Alessio.station>
2016-05-27 19:44:27 +0200

    Add extra application style

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -20,7 +20,7 @@ var Nav = React.createClass({
         <div className="top-bar-right">
           <form onSubmit={this.onSearch}>
             <ul className="menu">
-              <li><input type="search" placeholder="Search Weather"/></li>
+              <li><input type="search" placeholder="Search weather by city"/></li>
               <li><button type="submit" className="button">Search</button></li>
             </ul>
           </form>
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,8 +15,8 @@ var WeatherForm = React.createClass({
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type="text" ref="location"/>
-          <button className="expanded button">Get Weather</button>
+          <input type="search" ref="location" placeholder="Search weather by city"/>
+          <button className="expanded button hollow">Get Weather</button>
         </form>
       </div>
     );
